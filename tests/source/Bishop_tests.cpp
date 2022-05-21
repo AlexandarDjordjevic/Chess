5229e72115b22ae80e21dae6e925eed1f7b278b6
Aleksandar Djordjevic <alexandar1608@gmail.com>
2022-05-21 12:05:02 +0200

    Add CMake Code Coverage

--- tests/source/Bishop_tests.cpp
+++ tests/source/Bishop_tests.cpp
@@ -4,7 +4,7 @@
 #include "chess/Exceptions.hpp"
 
 
-class Bishop_movement_tests : public testing::Test {
+class Bishop_movement : public testing::Test {
     class Test_board_init : public chess::ABoard_initializer {
     public:
         Test_board_init() = default;
@@ -14,53 +14,53 @@ public:
     chess::Board board{std::make_shared<Test_board_init>()};
 };
 
-TEST_F(Bishop_movement_tests, ilegal_diagonal_move) {
+TEST_F(Bishop_movement, illegal_diagonal_move) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_THROW(board.move_piece({"a1:c7", chess::Color::white}), chess::exceptions::Invalid_move);
     ASSERT_NE(board[{"a1"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, ilegal_horizontal_move) {
+TEST_F(Bishop_movement, illegal_horizontal_move) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_THROW(board.move_piece({"a1:e1", chess::Color::white}), chess::exceptions::Invalid_move);
     ASSERT_NE(board[{"a1"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, ilegal_vertical_move) {
+TEST_F(Bishop_movement, illegal_vertical_move) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_THROW(board.move_piece({"a1:a7", chess::Color::white}), chess::exceptions::Invalid_move);
     ASSERT_NE(board[{"a1"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, valid_diagonal_move_rising_x_rising_y) {
+TEST_F(Bishop_movement, valid_diagonal_move_rising_x_rising_y) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_NO_THROW(board.move_piece({"a1:d4", chess::Color::white}));
     ASSERT_EQ(board[{"a1"}], nullptr);
     ASSERT_NE(board[{"d4"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, valid_diagonal_move_falling_x_faling_y) {
+TEST_F(Bishop_movement, valid_diagonal_move_falling_x_faling_y) {
     board[{"e6"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_NO_THROW(board.move_piece({"e6:a2", chess::Color::white}));
     ASSERT_EQ(board[{"e6"}], nullptr);
     ASSERT_NE(board[{"a2"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, valid_diagonal_move_rising_x_faling_y) {
+TEST_F(Bishop_movement, valid_diagonal_move_rising_x_faling_y) {
     board[{"b5"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_NO_THROW(board.move_piece({"b5:f1", chess::Color::white}));
     ASSERT_EQ(board[{"b5"}], nullptr);
     ASSERT_NE(board[{"f1"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, valid_diagonal_move_falling_x_rising_y) {
+TEST_F(Bishop_movement, valid_diagonal_move_falling_x_rising_y) {
     board[{"f1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_NO_THROW(board.move_piece({"f1:b5", chess::Color::white}));
     ASSERT_EQ(board[{"f1"}], nullptr);
     ASSERT_NE(board[{"b5"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, diagonal_move_with_enemy_figure_between_start_and_end) {
+TEST_F(Bishop_movement, diagonal_move_with_enemy_figure_between_start_and_end) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     board[{"d4"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::black);
     ASSERT_THROW(board.move_piece({"a1:h8", chess::Color::white}), chess::exceptions::Invalid_move);
@@ -68,7 +68,7 @@ TEST_F(Bishop_movement_tests, diagonal_move_with_enemy_figure_between_start_and_
     ASSERT_NE(board[{"d4"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, diagonal_move_with_ally_figure_between_start_and_end) {
+TEST_F(Bishop_movement, diagonal_move_with_ally_figure_between_start_and_end) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     board[{"e5"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_THROW(board.move_piece({"a1:h8", chess::Color::white}), chess::exceptions::Invalid_move);
@@ -76,14 +76,14 @@ TEST_F(Bishop_movement_tests, diagonal_move_with_ally_figure_between_start_and_e
     ASSERT_NE(board[{"e5"}], nullptr);
 }
 
-TEST_F(Bishop_movement_tests, diagonal_move_destination_occupied_with_ally_figure) {
+TEST_F(Bishop_movement, diagonal_move_destination_occupied_with_ally_figure) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_THROW(board.move_piece({"a1:c7", chess::Color::white}), chess::exceptions::Invalid_move);
     ASSERT_NE(board[{"a1"}], nullptr);
 }
 
 
-TEST_F(Bishop_movement_tests, diagonal_move_kill_enemy_figure) {
+TEST_F(Bishop_movement, diagonal_move_kill_enemy_figure) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::bishop, chess::Color::white);
     ASSERT_THROW(board.move_piece({"a1:c7", chess::Color::white}), chess::exceptions::Invalid_move);
     ASSERT_NE(board[{"a1"}], nullptr);
--- tests/source/Rook_tests.cpp
+++ tests/source/Rook_tests.cpp
@@ -4,7 +4,7 @@
 #include "chess/Exceptions.hpp"
 
 
-class Rook_movement_tests : public testing::Test {
+class Rook_movement : public testing::Test {
     class Test_board_init : public chess::ABoard_initializer {
     public:
         Test_board_init() = default;
@@ -14,13 +14,13 @@ public:
     chess::Board board{std::make_shared<Test_board_init>()};
 };
 
-TEST_F(Rook_movement_tests, ilegal_diagonal_move) {
+TEST_F(Rook_movement, illegal_diagonal_move) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     ASSERT_THROW(board.move_piece({"a1:c7", chess::Color::white}), chess::exceptions::Invalid_move);
     ASSERT_NE(board[{"a1"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, valid_vertical_move) {
+TEST_F(Rook_movement, valid_vertical_move) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     ASSERT_NE(board[{"a1"}], nullptr);
     ASSERT_NO_THROW(board.move_piece({"a1:a7", chess::Color::white}));
@@ -28,7 +28,7 @@ TEST_F(Rook_movement_tests, valid_vertical_move) {
     ASSERT_NE(board[{"a7"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, vertical_move_destination_occupied_with_ally) {
+TEST_F(Rook_movement, vertical_move_destination_occupied_with_ally) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"a7"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
 
@@ -37,7 +37,7 @@ TEST_F(Rook_movement_tests, vertical_move_destination_occupied_with_ally) {
     ASSERT_NE(board[{"a7"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, vertical_move_with_ally_figure_between_start_and_end) {
+TEST_F(Rook_movement, vertical_move_with_ally_figure_between_start_and_end) {
     board[{"c1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"c4"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
 
@@ -46,7 +46,7 @@ TEST_F(Rook_movement_tests, vertical_move_with_ally_figure_between_start_and_end
     ASSERT_NE(board[{"c4"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, vertical_move_with_enemy_figure_between_start_and_end) {
+TEST_F(Rook_movement, vertical_move_with_enemy_figure_between_start_and_end) {
     board[{"d1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"d2"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::black);
 
@@ -56,7 +56,7 @@ TEST_F(Rook_movement_tests, vertical_move_with_enemy_figure_between_start_and_en
     ASSERT_EQ(board[{"d5"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, vertical_move_kill_enemy_figure) {
+TEST_F(Rook_movement, vertical_move_kill_enemy_figure) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"a4"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::black);
 
@@ -66,7 +66,7 @@ TEST_F(Rook_movement_tests, vertical_move_kill_enemy_figure) {
     ASSERT_EQ(board[{"a4"}]->get_color(), chess::Color::white);
 }
 
-TEST_F(Rook_movement_tests, valid_horizontal_move) {
+TEST_F(Rook_movement, valid_horizontal_move) {
     board[{"f3"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     ASSERT_NE(board[{"f3"}], nullptr);
     ASSERT_NO_THROW(board.move_piece({"f3:a3", chess::Color::white}));
@@ -74,7 +74,7 @@ TEST_F(Rook_movement_tests, valid_horizontal_move) {
     ASSERT_EQ(board[{"f3"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, horizontal_move_destination_occupied_with_ally_figure) {
+TEST_F(Rook_movement, horizontal_move_destination_occupied_with_ally_figure) {
     board[{"b1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"f1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
 
@@ -83,7 +83,7 @@ TEST_F(Rook_movement_tests, horizontal_move_destination_occupied_with_ally_figur
     ASSERT_NE(board[{"f1"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, horizontal_move_with_ally_figure_between_start_and_end) {
+TEST_F(Rook_movement, horizontal_move_with_ally_figure_between_start_and_end) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"c1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
 
@@ -93,7 +93,7 @@ TEST_F(Rook_movement_tests, horizontal_move_with_ally_figure_between_start_and_e
     ASSERT_EQ(board[{"e1"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, horizontal_move_with_enemy_figure_between_start_and_end) {
+TEST_F(Rook_movement, horizontal_move_with_enemy_figure_between_start_and_end) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"c1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::black);
 
@@ -102,7 +102,7 @@ TEST_F(Rook_movement_tests, horizontal_move_with_enemy_figure_between_start_and_
     ASSERT_NE(board[{"c1"}], nullptr);
 }
 
-TEST_F(Rook_movement_tests, horizontal_move_kill_enemy_figure) {
+TEST_F(Rook_movement, horizontal_move_kill_enemy_figure) {
     board[{"a1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::white);
     board[{"c1"}] = chess::Piece_factory::create(chess::Piece_type::rook, chess::Color::black);
 
